c6252817e23029207adc09c254d3ec84d785a456
RipeCherries <alexeyegoshin0403@mail.ru>
2023-01-04 20:45:23 +0700

    Added functionality to the main page

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const sliderValue = document.querySelectorAll("span");
 const inputSlider = document.querySelector("input");
 
-inputSlider.oninput = function () {
+const changeSize = () => {
     const value = inputSlider.value;
     sliderValue[0].textContent = value;
     sliderValue[1].textContent = value;
 }
 
-localStorage.setItem("size", 8);
+const setSize = () => {
+    localStorage.clear();
+    localStorage.setItem("size", inputSlider.value);
+}
